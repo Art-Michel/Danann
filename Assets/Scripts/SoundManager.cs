d4f54be2fee553620bd09678f76d851604695a58
Arthur MSI <arthur.michel@e-artsup.net>
2022-04-26 11:55:08 +0200

    SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : ProjectManager<SoundManager>
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip hit;
+
+    private void PlaySound(AudioClip clip, float volume)
+    {
+        audioSource.PlayOneShot(clip, volume);
+    }
+
+    public void PlayHit()
+    {
+        PlaySound(hit, 1f);
+    }
+    
+
+}
