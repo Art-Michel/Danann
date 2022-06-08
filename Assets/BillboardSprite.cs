c6183e059a10fd03f889a2e4c4def9ab3e8e90a6
Arthur MSI <arthur.michel@e-artsup.net>
2022-06-08 13:11:56 +0200

    BillboardSprite.cs

---
+++ Assets/BillboardSprite.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardSprite : MonoBehaviour
+{
+    Transform _camera;
+
+    void Awake()
+    {
+        _camera = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        transform.forward = _camera.position - transform.position;
+    }
+}
